d906e34caadac148601346611e58c89fa0134a87
FabioVenu <fabio.venuto@outlook.it>
2016-01-30 12:26:50 +0100

    Joystick movement also for EvilCloud

--- GGJ2016/Assets/Scripts/EvilCloudMovement.cs
+++ GGJ2016/Assets/Scripts/EvilCloudMovement.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class EvilCloudMovement : MonoBehaviour {
 
-    Vector3 speed = Vector3.zero;
+    Vector3 vectorSpeed = Vector3.zero;
+    public bool joystickInserted = true;
+    public float speed = 5;
     public float fixedZ = 2;
     float smoothTime = 1;
     Rigidbody2D rb2d;
+
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
@@ -14,15 +17,35 @@ public class EvilCloudMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (joystickInserted)
+        {
+            JoystickController();
+        }
+        else
+        {
+            MouseController();
+        }
+        
+        //rb2d.MovePosition(rb2d.position + )
+        
+       
+	}
+
+    void MouseController()
+    {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
         gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0;
         mousePosition = new Vector3(mousePosition.x, mousePosition.y, fixedZ);
-        transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref speed, smoothTime);
-           
-        //rb2d.MovePosition(rb2d.position + )
-        
-	}
+        transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref vectorSpeed, smoothTime);
+    }
+
+    void JoystickController()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("HorizontalCloud"), Input.GetAxisRaw("VerticalCloud"));
+        rb2d.MovePosition(rb2d.position + input * speed * Time.deltaTime);
+    }
 }
