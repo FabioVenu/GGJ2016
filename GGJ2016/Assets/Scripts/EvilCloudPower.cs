a714a91aa5ece384c3a09e7b872c81fc4271877d
FabioVenu <fabio.venuto@outlook.it>
2016-01-30 15:46:41 +0100

    hole prefab

--- GGJ2016/Assets/Scripts/EvilCloudPower.cs
+++ GGJ2016/Assets/Scripts/EvilCloudPower.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class EvilCloudPower : MonoBehaviour {
     public GameObject iceTile;
+
 	// Use this for initialization
 	void Start () {
 	    
@@ -11,6 +12,12 @@ public class EvilCloudPower : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1);
+            Instantiate(iceTile, position, transform.rotation);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
         {
             Vector3 position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1);
             Instantiate(iceTile, position, transform.rotation);
