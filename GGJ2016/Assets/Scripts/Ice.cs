397ab45ca9d1947742deac60d3820839328d5199
FabioVenu <fabio.venuto@outlook.it>
2016-01-30 13:42:32 +0100

    Ice prefabs

---
+++ GGJ2016/Assets/Scripts/Ice.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<PlayerMovement>().FixInput();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<PlayerMovement>().UnfixInput();
+        }
+    }
+}
