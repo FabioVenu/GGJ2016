bb0bcc48f6e05b8c5010477d42ad252c11ca8d17
panda <sb@tabulatouch.eu>
2016-01-30 14:59:50 +0100

    GameManager and enemy Respawn

--- GGJ2016/Assets/Scripts/EvilCloudMovement.cs
+++ GGJ2016/Assets/Scripts/EvilCloudMovement.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class EvilCloudMovement : MonoBehaviour {
 
     Vector3 vectorSpeed = Vector3.zero;
-    public bool joystickInserted = true;
-    public float speed = 5;
-    public float fixedZ = 2;
-    float smoothTime = 1;
-    Rigidbody2D rb2d;
+    public bool     joystickInserted = true;
+    public float    speed = 5;    
+    float           smoothTime = 1;
+    Rigidbody2D     rb2d;
+    SpriteRenderer spriterenderer;
+
+    public float    LifeTime = 5;
+
+    private float   StartTime=-1, EndTime=-1;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -26,11 +31,15 @@ public class EvilCloudMovement : MonoBehaviour {
         {
             MouseController();
         }
+
+        var t = Time.time;
+
+        if (IsTimeEnded)
+            Respawn();
         
-        //rb2d.MovePosition(rb2d.position + )
-        
-       
-	}
+        // Transparency dependent on LifeTime
+        spriterenderer.color = new Color(spriterenderer.color.r, spriterenderer.color.g, spriterenderer.color.b, 1.0f - TimePercentage);
+    }
 
     void MouseController()
     {
@@ -39,13 +48,49 @@ public class EvilCloudMovement : MonoBehaviour {
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
         gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0;
-        mousePosition = new Vector3(mousePosition.x, mousePosition.y, fixedZ);
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, GameManager.FixedZ);
         transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref vectorSpeed, smoothTime);
     }
 
     void JoystickController()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("HorizontalCloud"), Input.GetAxisRaw("VerticalCloud"));
-        rb2d.MovePosition(rb2d.position + input * speed * Time.deltaTime);
+        try
+        {
+            Vector2 input = new Vector2(Input.GetAxisRaw("HorizontalCloud"), Input.GetAxisRaw("VerticalCloud"));
+            rb2d.MovePosition(rb2d.position + input * speed * Time.deltaTime);
+        }
+        catch
+        {}
+    }
+
+    // will return true if our time has finished
+    public bool IsTimeEnded
+    {
+        get
+        {
+            return StartTime==-1 || Time.time >= EndTime;
+        }
+    }
+
+    public float TimePercentage
+    {
+        get
+        {
+            return (Time.time - StartTime) / (EndTime - StartTime);
+        }
+    }
+
+    // Will give a new position/time to the evil cloud
+    public void Respawn()
+    {
+        StartTime = Time.time;
+        EndTime = Time.time + LifeTime;
+
+        //TODO: get new position from manager
+        transform.localPosition = GameManager.getRandomEvilCloudPosition();
+
+
+
+
     }
 }
---
+++ GGJ2016/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject EvilCloud;
+
+    public static float FixedZ = -5;
+
+    [HideInInspector]
+    public static GameManager Instance;
+
+	// Use this for initialization
+	void Start () {
+
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    // Will return a new position for the evil cloud, 
+    public static Vector3 getRandomEvilCloudPosition()
+    {
+        Vector2 direction = new Vector2(Random.value, Random.value).normalized;
+        Vector2 newpos = new Vector2(Instance.Player.transform.position.x, Instance.Player.transform.position.y) + direction * Random.Range(0.1f, 10);
+
+        return new Vector3(newpos.x, newpos.y, FixedZ);
+    }
+
+}
