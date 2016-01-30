4a1cd711dc08084d0c15d05e4faac88ee036a25a
panda <sb@tabulatouch.eu>
2016-01-30 13:29:21 +0100

    FIxes in camera

--- GGJ2016/Assets/Scripts/CameraManager.cs
+++ GGJ2016/Assets/Scripts/CameraManager.cs
@@ -6,10 +6,11 @@ public class CameraManager : MonoBehaviour {
     public GameObject player;
     public GameObject evilCloud;
     public float fixedZ = -10;
-    public float CameraCenterAverage = 0.5f; // centrato tra i due punti
+    public float CameraCenterAverage = 0.2f; // more centered towards Player
     public float CameraMinZoomOutSize = 6;        
-    public float CameraMaxZoomOutSize = 10;       
-    public float CameraZoomSpeed = 1;           
+    public float CameraMaxZoomOutSize = 22;       
+    public float CameraZoomInTime = 1;
+    public float CameraZoomOutTime = 0.5f;
     Camera cam;
 
     private float CameraZoomCurrentVelocity = 0.0f;
@@ -31,15 +32,22 @@ public class CameraManager : MonoBehaviour {
         bool evilCloudVisible = (wp2.x >= 0 && wp2.x <= 1) && (wp2.y >= 0 && wp2.y <= 1);
 
         float cameraSize = CameraMinZoomOutSize;
+        float cameraZoomSpeed = 0;
         if (!playerVisible || !evilCloudVisible)
+        {
             cameraSize = CameraMaxZoomOutSize;
+            cameraZoomSpeed = CameraZoomOutTime;
+        }
         else
+        {
             cameraSize = CameraMinZoomOutSize;
+            cameraZoomSpeed = CameraZoomInTime;
+        }
 
         Vector2 camerapos = Vector2.Lerp(new Vector2(playerPos.x, playerPos.y), new Vector2(evilCloudPos.x, evilCloudPos.y), CameraCenterAverage);
 
         transform.position = new Vector3(camerapos.x, camerapos.y, fixedZ);
-        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, cameraSize, ref CameraZoomCurrentVelocity, CameraZoomSpeed);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, cameraSize, ref CameraZoomCurrentVelocity, cameraZoomSpeed);
             
     }
 }
