83cd82a045f015aecf8f44b937c38cbe556bdbb2
FabioVenu <fabio.venuto@outlook.it>
2016-01-30 02:35:21 +0100

    Movement

---
+++ GGJ2016/Assets/Scripts/EvilCloudMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvilCloudMovement : MonoBehaviour {
+
+    Vector3 speed = Vector3.zero;
+    public float fixedZ = 2;
+    float smoothTime = 1;
+    Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0;
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, fixedZ);
+        transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref speed, smoothTime);
+           
+        //rb2d.MovePosition(rb2d.position + )
+        
+	}
+}
---
+++ GGJ2016/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.MovePosition(rb2d.position + input * speed * Time.deltaTime);
+	}
+}
